c27ebac00285a8bb9b2cae9338846264eb719207
siluthfi <luthfirpl.skenda@gmail.com>
2022-01-29 14:29:38 +0700

    changed a method of getting a data from reducer using useSelector instead mapStateToProps

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,21 +1,13 @@
-import { Component} from "react";
 import BandEach from "./BandEach";
-import { connect } from 'react-redux';
+import { useSelector } from "react-redux";
 
-class Bands extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                <BandEach characters={this.props.bands} />
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-        bands: state.characters
-    }
+const Bands = () => {
+    const bands = useSelector(state => state.characters)
+    return (
+        <div className="container-fluid">
+            <BandEach characters={bands} />
+        </div>
+    );
 }
  
-export default connect(mapStateToProps)(Bands);
+export default Bands;
--- src/components/Chara.js
+++ src/components/Chara.js
@@ -1,25 +1,35 @@
 import { Link, useParams } from "react-router-dom";
 import DataMember from "./data/DataMember";
 import DataBand from "./data/DataBand";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Chara = () => {
+    const [name, setName] = useState('');
     const { band, member } = useParams()
-    const { band: bandname, hash } = DataBand(band)
-    const { name, role } = DataMember(member)
-    console.log(bandname, hash)
+    const eachmember = useSelector(state => state.characters)
+                        .filter(chara => chara.slug === band)
+                        .filter(band => band.nameSlug === member)
+                        .map(data => data)[0];
+
+    /* using custom hooks 
+        const { band: bandname, hash } = DataBand(band)
+        const { name, role } = DataMember(member)
+    */
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-3 offset-md-1">
-                    <img src={`/img/${member}.png`} alt="" className="img-fluid shadow" />
+                    <img src={`/img/${eachmember.nameSlug}.png`} alt="" className="img-fluid shadow" />
                 </div>
                 <div className="col-md-7 bg-white shadow">
-                    {/* <h1 className="text-center display-3 mt-5">{name}</h1> */}
+                    <h1 className="text-center display-3 mt-5"></h1>
                 </div>
             </div>
             <div className="row my-5">
                 <div className="col-md-3 offset-md-1">
-                    <Link to={`/bands#${hash}`} className="btn btn-custom shadow-sm">Back</Link>
+                    <Link to={`/bands#${eachmember.slug}`} className="btn btn-custom shadow-sm">Back</Link>
                 </div>
             </div>
         </div>
